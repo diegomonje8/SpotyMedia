c2a5e4ca0141a4f7738342f15e7662d30a5ab50f
Diego <diegomonje8@gmail.com>
2021-02-22 23:50:16 +0100

    F006 Base Parent

--- app/src/main/java/es/nauticapps/spotymedia/ui/MainActivity.kt
+++ app/src/main/java/es/nauticapps/spotymedia/ui/MainActivity.kt
@@ -2,6 +2,9 @@ package es.nauticapps.spotymedia.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import es.nauticapps.spotymedia.R
 import es.nauticapps.spotymedia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +13,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
